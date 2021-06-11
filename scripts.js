1f3818fabedbc343c611ee45f43bf4e0cde7e41f
WillieC92 <82012321+WillieC92@users.noreply.github.com>
2021-06-11 12:25:37 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const friendsNames = ["Jarvis", "Larry", "Josh", "Devan"];
+
+const locationNames = ["Singapore", "Scotland", "My Cousin's House", "Valhalla","Ohio", "China", "India", "Insval","Resenbol", "Dublin"];
+
+const murderWeapons = ["Can of Soup", "Bag of ToeNails","Issa a Knife","Mike Tyson","Chipotle","A shoe","Chapstick","My Ex","Mauser C78 Zig-Zag",
+"Mauser C96", "Nagant"
+,"M1895","Berdan M1870","Gras M1874","Mauser M1887","Mosin–Nagant M1891","Peabody–Martini–Henry M1874","Shintō","Katana","Rapier","Morning star","Flanged mace",]; 
+
+
+
